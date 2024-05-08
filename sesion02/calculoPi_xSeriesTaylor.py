5e31534906cae5945e8c37d736635e08f2ef7bf8
JEMlcr19 <jeduardomalacara@gmail.com>
2024-05-08 11:26:25 -0600

    Segunda clase

---
+++ sesion02/calculoPi_xSeriesTaylor.py
@@ -0,0 +1,20 @@
+import math
+acumulador = 0.0
+division_acual = 1.0
+contador = 0
+
+while division_acual > 0.0052:
+    contador += 1
+    division_acual = 1/contador
+
+    if contador % 2 != 0:
+        if essuma:
+            acumulador += division_acual
+        else:
+            acumulador -= division_acual
+        essuma = not essuma
+
+print("El cuarto del area es: ",acumulador)
+pi = acumulador * 4
+print("Pi es igual a: ", pi)
+
